c7f2aef87863ea737785e808af039664ddbc38d8
winnieotieno770 <winnieotieno770@gmail.com>
2025-12-31 16:43:31 +0300

    implement long_planeteer_calls

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -5,8 +5,14 @@ def roll_call_dwarves(dwarfs):
 def summon_captain_planet(planeteer_calls):
    p= [f"{planet[0].upper()}{''.join(planet[1:])}" for planet in planeteer_calls]
    return p
-def long_planeteer_calls():
-    pass
-
+def long_planeteer_calls(words):
+    long = 4
+    for word in words:
+        if len(word) > long:
+            return True
+    return False
+        
+        
+long_planeteer_calls(['go', 'to'])
 def find_the_cheese():
     pass
