76557b578f16d9e830aa48cf86d0b07ee02e00ce
winnieotieno770 <winnieotieno770@gmail.com>
2025-12-31 16:29:24 +0300

    feat: add summon_captain_planet with roll_call

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,9 +1,10 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    for i,dwarf in enumerate(dwarfs):
+        print(f"{i+1}. {dwarf}")
 
-def summon_captain_planet():
-    pass
-
+def summon_captain_planet(planeteer_calls):
+   p= [f"{planet[0].upper()}{''.join(planet[1:])}" for planet in planeteer_calls]
+   return p
 def long_planeteer_calls():
     pass
 
